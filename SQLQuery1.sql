a18509deea07f108b285d2406ff3355f33a38cb4
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-08-19 18:48:06 +0530

    [Ariprasath] added: retreveing contacts of particular city or state

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,7 +3,7 @@
 USE AddressBook;
 
 CREATE TABLE AddressBookContacts(
-firstName varchar(255),
+firstName varchar(255) unique,
 lastName varchar(255),
 address varchar(255),
 city varchar(255),
@@ -14,9 +14,12 @@ email varchar(255)
 );
 
 INSERT INTO dbo.AddressBookContacts values
-('Ariprasath', 'Sakthivel', '3/6, 4th main road', 'Bangalore', 'karnataka', '560085', '1235678900', 'ariprasath3@gmail.com'),
-('Raj', 'Kumar', '51, 2nd main road', 'Bangalore', 'karnataka', '560035', '12121215536', 'adas@gmail.com');
+('Ariprasath', 'Sakthivel', '3/6, 4th main road', 'Bangalore', 'karnataka', 560085, '1235678900', 'ariprasath3@gmail.com'),
+('Raj', 'Kumar', '51, 2nd main road', 'Bangalore', 'karnataka', 560035, '12121215536', 'adas@gmail.com'),
+('Ragu', 'ram', '50, 1st main', 'chennai', 'Tamil Nadu', 636808, '1212333655', 'abc@gmail.com')
 
 UPDATE dbo.AddressBookContacts SET address='413, 5th main road' WHERE firstName='Ariprasath';
 
 DELETE FROM dbo.AddressBookContacts WHERE firstName='Raj';
+
+SELECT * FROM dbo.AddressBookContacts WHERE city='Bangalore' OR state='Tamil Nadu';
