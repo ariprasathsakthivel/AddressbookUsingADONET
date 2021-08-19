c482883931d6f6fa8c00fb085f269441b1be20b0
ariprasathsakthivel <67471289+ariprasathsakthivel@users.noreply.github.com>
2021-08-19 18:07:32 +0530

    [Ariprasath] added: create a table for storing the contact details- firstname, lastname, address, city, state, zipcode, phone number, email

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,1 +1,11 @@
 ﻿CREATE DATABASE AddressBook;
+CREATE TABLE AddressBookContacts(
+firstName varchar(255),
+lastName varchar(255),
+address varchar(255),
+city varchar(255),
+state varchar(255),
+zipCode int,
+phoneNumber varchar(13),
+email varchar(255)
+);
